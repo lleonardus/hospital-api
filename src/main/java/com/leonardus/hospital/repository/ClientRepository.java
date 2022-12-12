90e67dcce0ad9cdee90e8218c8ca26bc45d3e664
leonardus <leonardus.wizard@gmail.com>
2022-12-12 11:56:35 -0300

    Criando ClientRepository

---
+++ src/main/java/com/leonardus/hospital/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.leonardus.hospital.repository;
+
+import com.leonardus.hospital.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
